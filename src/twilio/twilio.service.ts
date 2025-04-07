2a4f20a7f82829009ab5bc5a09921c2f599e097e
Younes Laaroussi <01ylaaroussi01@gmail.com>
2025-04-07 15:25:39 -0300

    throw correct error message for missing env vars

--- src/twilio/twilio.service.ts
+++ src/twilio/twilio.service.ts
@@ -1,26 +1,29 @@
+import { Twilio } from 'twilio';
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Twilio } from 'twilio';
 import { Environment } from '../common/enums/environment.enum';
 
 @Injectable()
 export class TwilioService {
     private readonly logger = new Logger(TwilioService.name);
-    private twilioClient: Twilio;
-    private twilioPhoneNumber: string;
+    private readonly twilioClient: Twilio;
+    private readonly twilioPhoneNumber: string;
 
-    constructor(private configService: ConfigService) {
-        const accountSid = this.configService.get<string>(Environment.TWILIO_ACCOUNT_SID);
-        const authToken = this.configService.get<string>(Environment.TWILIO_AUTH_TOKEN);
-        this.twilioPhoneNumber = this.configService.get<string>(Environment.TWILIO_PHONE_NUMBER);
+    constructor(private readonly configService: ConfigService) {
+        const accountSid = this.getRequiredConfig(Environment.TWILIO_ACCOUNT_SID);
+        const authToken = this.getRequiredConfig(Environment.TWILIO_AUTH_TOKEN);
+        this.twilioPhoneNumber = this.getRequiredConfig(Environment.TWILIO_PHONE_NUMBER);
 
-        if (!accountSid || !authToken || !this.twilioPhoneNumber) {
-            throw new Error('Twilio configuration is missing!');
-        }
 
         this.twilioClient = new Twilio(accountSid, authToken);
         this.logger.log('Twilio client initialized.');
     }
+
+    private getRequiredConfig(key: Environment): string {
+        const val = this.configService.get<string>(key);
+        if (!val) throw new Error('Twilio configuration is missing!');
+        return val;
+   }
 
     getClient(): Twilio {
         return this.twilioClient;
